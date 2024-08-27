ff639faa53956919075eeb8140032a06a1e1425a
Jamie-McGregor0 <jamiexie06@outlook.com>
2024-08-27 07:10:57 +0000

    Tue, Aug 27, 2024, 3:10 PM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,10 +9,10 @@ int main(void){
 
     for(int i = 0; i < n; i ++;){
 
+        sum  = sum + n % 10;
         n = n / 10;
         x = n % 10 * 2;
         n = n / 10;
-
         sum = sum + x / 10 + x % 10;
 
 
