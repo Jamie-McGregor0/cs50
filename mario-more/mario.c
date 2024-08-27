2aa978a33396b8aede7b2db6091647e32121d818
Jamie-McGregor0 <jamiexie06@outlook.com>
2024-08-27 06:33:13 +0000

    Tue, Aug 27, 2024, 2:33 PM +08:00

---
+++ mario-more/mario.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void){
+
+    int h;
+    do{
+        h = get_int("Height: ");
+    }while(h < 1);
+
+    int w = 2 * h + 2;
+
+    for(int i = 0; i < h; i++){
+
+        int s = (w / 2 - 2 - i); //space in one side
+
+        for(int j = 0; j < w; j++){
+
+            if ((j >= s && j < w/2 - 1) || (j > w/2 && j < w - s))
+                printf("#");
+
+            else if(j < w - s)
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+
+}
