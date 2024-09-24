bb171c87c707cd332cf131230e10297dfc61d757
Jamie-McGregor0 <jamiexie06@outlook.com>
2024-09-24 12:16:27 +0000

    Tue, Sep 24, 2024, 8:16 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,12 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
 
-    string sen = get_string();
+    string sen = get_string("Text: ");
+
+    int length = strlen(sen);
+
+
 
     printf("%s\n", sen);
-
+    printf("%i\n", length);
 
 
 }
