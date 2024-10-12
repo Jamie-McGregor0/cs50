47e89d4781d2e741ede341fc224ba1cb165a82f0
Jamie-McGregor0 <jamiexie06@outlook.com>
2024-10-12 12:51:00 +0000

    Sat, Oct 12, 2024, 8:51 PM +08:00

---
+++ substitution/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[]){
+
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    string key = argv[argc - 1];
+
+…
+}
