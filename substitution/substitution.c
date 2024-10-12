c52657ea5bf35727c327594ab1706c1c660f6b80
Jamie-McGregor0 <jamiexie06@outlook.com>
2024-10-12 12:00:48 +0000

    Sat, Oct 12, 2024, 8:00 PM +08:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,19 +6,16 @@
 int main(int argc, string argv[]){
 
     string key = argv[argc - 1];
-    int check = 0;
 
     for(int i = 0; i < strlen(key); i++){
 
         if(isalpha(key[i]) == 0){
-
-            check += 1;
-            
+            printf("Usage: %s key\n", argv[0]);
+            return 1;
         }
     }
 
-    if(check != 0)
-        printf("Usage: %s key\n", argv[0]);
+    
 
 
 
