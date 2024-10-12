f853136e83129967d26e1efe00cffef310b756b3
Jamie-McGregor0 <jamiexie06@outlook.com>
2024-10-12 13:21:51 +0000

    Sat, Oct 12, 2024, 9:21 PM +08:00

--- substitution/test.c
+++ substitution/test.c
@@ -11,9 +11,9 @@ int main(void){
 
     string plain = get_string("plaintext: ");
 
-    string cipher[strlen(plain)];
+    string cipher[1];
 
-    strcpy(cipher, plain);
+    cipher = strcpy(cipher, plain);
 
     for(int i = 0; i < strlen(plain); i++){
 
