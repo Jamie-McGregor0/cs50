cbf446c51cf2f012cec78ba6cf8c237fde920503
Jamie-McGregor0 <jamiexie06@outlook.com>
2024-12-28 13:25:40 +0000

    Sat, Dec 28, 2024, 9:25 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -118,14 +118,14 @@ bool vote(int rank, string name, int ranks[])
 void record_preferences(int ranks[])
 {
     // TODO
-    for (int i = 0; i < candidate_count - 1; i ++)
+    for (int i = 0; i < candidate_count; i ++)
     {
-        for (int j = i + 1; j < candidate_count; j ++)
+        for (int j = 0; j < candidate_count; j ++)
         {
             for (int k = 0; k < candidate_count; k ++)
             {
                 if (candidates[i], strcmp(ranks[k]) && strcmp(candidates[j], ranks[k + 1]))
-                preferences[i][j] += 1;
+                    preferences[i][j] += 1;
             }
         }
     }
