12201bfab3b6583e3b3f52e4444917a5d2640d06
Jamie-McGregor0 <jamiexie06@outlook.com>
2024-12-29 10:29:13 +0000

    Sun, Dec 29, 2024, 6:29 PM +08:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -140,11 +140,11 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
-    for (int i = 0; i < candidate_count; i ++)
+    for (int k = 0; k < MAX * (MAX - 1) / 2; k ++)
     {
         for (int j = 0; j < candidate_count; j ++)
         {
-            for( int k = 0; k < MAX * (MAX - 1) / 2; k ++)
+            for (int i = 0; i < candidate_count; i ++)
             {
                 if (preferences[i][j] > preferences[j][i])
                 {
