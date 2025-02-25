f814652ac05a67fa441c46e115f0561a49545240
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-02-25 13:09:40 +0000

    Tue, Feb 25, 2025, 9:09 PM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,10 +35,9 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
-    while (fread(&header, HEADER_SIZE, 1, argv[1]) != 0)
-    {
-        fwrite(&header, HEADER_SIZE, 1, argv[2]);
-    }
+    fread(&header, HEADER_SIZE, 1, input)
+    fwrite(&header, HEADER_SIZE, 1, output);
+
 
     // TODO: Read samples from input file and write updated data to output file
 
