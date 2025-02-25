7d965d50d0fbb82e92dd2f00ce06365eff1643a3
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-02-25 12:49:34 +0000

    Tue, Feb 25, 2025, 8:49 PM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,8 +35,7 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     char *header = malloc(HEADER_SIZE);
-
-    for(int i = 0; i < HEADER)
+    *header = &
 
     // TODO: Read samples from input file and write updated data to output file
 
