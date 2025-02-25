404cd80b6ba5b591a1d48c913e4a74ca08a48c86
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-02-25 13:14:13 +0000

    Tue, Feb 25, 2025, 9:14 PM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -39,7 +39,12 @@ int main(int argc, char *argv[])
     fwrite(&header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
-    int16_t sample;
+    int16_t buffer;
+
+    w
+
+
+
     // Close files
     fclose(input);
     fclose(output);
