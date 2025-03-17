f92ca8e896826e46221a6b54dae2b10abf856855
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-03-17 11:17:29 +0000

    Mon, Mar 17, 2025, 7:17 PM +08:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -75,5 +75,70 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp[height][width];
+    for (int i = 0; i < height; i ++){
+        for (int j = 0; j < width; j ++){
+            temp[i][j] = image[i][j];
+
+        }
+    }
+
+    typedef struct{
+        int blue;
+        int green;
+        int red;
+}g;
+
+
+    for (int i = 0; i < height; i ++){
+        for (int j = 0; j < width; j ++){
+
+            g gx = {0,0,0};
+            g gy = {0,0,0};
+
+            for (int k = -1; k < 2; k ++){
+                for (int l = -1; l < 2; l ++){
+                    if (i + k >= 0 && i + k < height && j + l >= 0 && j + l < width){
+                        if (k >= 0){
+                            gx.blue += temp[i + k][j + l].rgbtBlue * (2 - k) * l;
+                            gx.green += temp[i + k][j + l].rgbtGreen * (2 - k) * l;
+                            gx.red += temp[i + k][j + l].rgbtRed * (2 - k) * l;
+                        }
+
+                        else{
+                            gx.blue += temp[i + k][j + l].rgbtBlue * l;
+                            gx.green += temp[i + k][j + l].rgbtGreen * l;
+                            gx.red += temp[i + k][j + l].rgbtRed * l;
+                        }
+
+                        if (l >= 0){
+                            gy.blue += temp[i + k][j + l].rgbtBlue * (2 - l) * k;
+                            gy.green += temp[i + k][j + l].rgbtGreen * (2 - l) * k;
+                            gy.red += temp[i + k][j + l].rgbtRed * (2 - l) * k;
+                        }
+
+                        else{
+                            gy.blue += temp[i + k][j + l].rgbtBlue * k;
+                            gy.green += temp[i + k][j + l].rgbtGreen * k;
+                            gy.red += temp[i + k][j + l].rgbtRed * k;
+                        }
+                    }
+                }
+            }
+
+            image[i][j].rgbtBlue = fmin(round(sqrt((pow(gx.blue, 2) + pow(gy.blue, 2)))), 255);
+            image[i][j].rgbtGreen = fmin(round(sqrt((pow(gx.green, 2) + pow(gy.green, 2)))), 255);
+            image[i][j].rgbtRed = fmin(round(sqrt((pow(gx.red, 2) + pow(gy.red, 2)))), 255);
+
+
+
+        }
+    }
+
+
+
+
+
+
     return;
 }
