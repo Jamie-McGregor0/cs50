7fe9c662b1e06ba8885a50486adcc6938b98e476
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-05-26 11:24:44 +0000

    Mon, May 26, 2025, 7:24 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -23,9 +23,9 @@ int main(int argc, char *argv[])
     char img_name;
 
     while(fread(buffer, 1, 512, card) == block_size){
-
-
-        if(buffer[i] == 0xff && buffer[i+1] == 0xd8 && buffer[i+2] == 0xff && (buffer[i+3] & 0xf0) == 0xe0){
+        for(int i = 0; i < block_size - 3; i ++){
+            
+            if(buffer[i] == 0xff && buffer[i+1] == 0xd8 && buffer[i+2] == 0xff && (buffer[i+3] & 0xf0) == 0xe0){
             if(jpg_count == 0){
                 sprintf(img_name, "%03i.jpg", jpg_count);
 
@@ -36,6 +36,13 @@ int main(int argc, char *argv[])
             else{
                 fclose(img);
             }
+
+
+
+
+        }
+
+
 
 
 
