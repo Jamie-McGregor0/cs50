b9ede2f6a74aff7a0f3c6196d702c649211a5cb4
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-05-26 10:36:38 +0000

    Mon, May 26, 2025, 6:36 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -21,10 +21,12 @@ int main(int argc, char *argv[])
     i == 0;
 
     while(fread(buffer, 1, 512, card) == 512){
+
+        char img_name[512];
         if(buffer[i] == 0xff && buffer[i+1] == 0xd8 && buffer[i+2] == 0xff && (buffer[i+3] & 0xf0) == 0xe0){
             if(i == 0){
-                FILE *img;
-                sprintf(img, "%03i.jpg", i);
+
+                sprintf(img_name[i], "%03i.jpg", i);
                 FILE *card = fopen(argv[1], "w");
                 fwrite(img, 1, 1, img0);
             }
