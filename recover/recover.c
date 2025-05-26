61bc9be7d7e464615adb7bba3e226e816f0b86c4
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-05-26 11:31:25 +0000

    Mon, May 26, 2025, 7:31 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -26,12 +26,13 @@ int main(int argc, char *argv[])
         for(int i = 0; i < block_size - 3; i ++){
 
             if(buffer[i] == 0xff && buffer[i+1] == 0xd8 && buffer[i+2] == 0xff && (buffer[i+3] & 0xf0) == 0xe0){
-            if(current_file != NULL){
-                fclose(current_file);
-                current_file = NULL;
-            }
+                if(current_file != NULL){
+                    fclose(current_file);
+                    current_file = NULL;
+                }
 
-            jpg_count ++;
+                jpg_count ++;
+
                 sprintf(img_name, "%03i.jpg", jpg_count);
 
                 FILE *output_file = fopen(argv[1], "w");
