11d81e7014f55ed005bc43cd8af1410ba31cb494
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-05-29 05:25:43 +0000

    Thu, May 29, 2025, 1:25 PM +08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -52,7 +52,7 @@ person *create_family(int generations)
         new_person->parent[0] = parent0;
         new_person->parent[1] = parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new_person-> = random() % 2;
+        new_person->alleles[0] = person0->alleles[random() % 2];
     }
 
     // If there are no generations left to create
