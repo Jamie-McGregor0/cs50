ec8d8ccd2925c1bf6646ad5b4891ce58718465b9
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-05-29 05:38:29 +0000

    Thu, May 29, 2025, 1:38 PM +08:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -76,10 +76,15 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    free()
+    if (p == NULL){
+        return 1;
+    }
     // TODO: Free parents recursively
+    free_family(p->parent[0]);
+    free_family(p->parent[1]);
 
     // TODO: Free child
+    free(p);
 }
 
 // Print each family member and their alleles.
