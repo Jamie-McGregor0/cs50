6c2337574f1bc4f88bd5882c672b87a1615e4a6c
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-05-30 13:59:15 +0000

    Fri, May 30, 2025, 9:59 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -92,7 +92,7 @@ bool unload(void)
         node *tmp = table[i];
         node *n = table[i];
 
-        if (table[i]->next != NULL)
+        while (table[i]->next != NULL)
         {
             n = n->next;
             free(tmp);
