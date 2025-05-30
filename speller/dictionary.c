d8e81910c23b709b4bcff88d0452e6897ae5294a
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-05-30 14:27:03 +0000

    Fri, May 30, 2025, 10:27 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -28,7 +28,7 @@ bool check(const char *word)
     // TODO
     for (node *n = table[hash(word)]; n->next != NULL; n = n->next)
     {
-        if (strcmp(n->word, tolower(word)) ==  0)
+        if (strcmp(n->word, strlwr(word)) ==  0)
         {
             return true;
         }
