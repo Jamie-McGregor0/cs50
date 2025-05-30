40b3ce6c7008c6f1cd8f13a82f5fecde9d690c00
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-05-30 08:51:51 +0000

    Fri, May 30, 2025, 4:51 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -56,7 +56,7 @@ bool load(const char *dictionary)
 
         strcpy(n->word, word);
 
-        n->next = table[hash(n->word)];
+        n->next = table[hash(n->word)].word;
         table[hash(n->word)] = n;
 
 
