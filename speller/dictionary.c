14b17725c9a1b431f2f76d8c35e65cd81aff5ec5
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-05-30 13:01:26 +0000

    Fri, May 30, 2025, 9:01 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -26,7 +26,7 @@ unsigned int dict_size = 0;
 bool check(const char *word)
 {
     // TODO
-    for (; table[hash(*word)]->next != NULL; *word = word->next)
+    for (node *n = ; table[hash(*word)]->next != NULL; *word = word->next)
     {
         if (strcasecmp(word.word, ))
     }
