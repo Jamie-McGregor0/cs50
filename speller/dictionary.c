0b5a3b5ebcf30baa7889ace4912e83d1aedb3df4
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-05-30 09:26:12 +0000

    Fri, May 30, 2025, 5:26 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -43,7 +43,10 @@ bool load(const char *dictionary)
         return false;
     }
 
-    char word[999];
+    char word_buffer[LENGTH + 1];
+    
+
+
 
     while (fscanf(dict, "%s", word) == 1)
     {
