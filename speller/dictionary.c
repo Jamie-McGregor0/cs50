73ca2200e7212352cce3faba7b741d8e89033736
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-05-30 08:05:48 +0000

    Fri, May 30, 2025, 4:05 PM +08:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -28,7 +28,8 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
+    // TODO: Improve this hash function\
+    
     return toupper(word[0]) - 'A';
 }
 
