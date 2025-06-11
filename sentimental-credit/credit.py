71d6fc26727be75fe0e2ed3a45bc6b76bc5bdbfe
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-06-11 18:06:25 +0000

    Thu, Jun 12, 2025, 2:06 AM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -4,15 +4,19 @@ n = get_string("Number: ")
 
 length = len(n)
 
-sum = 0
+sum1 = 0
+sum2 = 0
 
 for i in range(length):
     if i % 2 == 0:
         m = int(n[i]) * 2
         if  m >= 10:
-            sum += m // 10 + m % 10
+            sum1 += m // 10 + m % 10
         else:
-            sum += m
+            sum1 += m
+
+    else:
+        
 
 
 print(sum)
