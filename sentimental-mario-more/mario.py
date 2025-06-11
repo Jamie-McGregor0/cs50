633d13f8ab9d7a558c4c1fde27400db8b62f41d4
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-06-11 14:54:21 +0000

    Wed, Jun 11, 2025, 10:54 PM +08:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -2,5 +2,8 @@ from cs50 import get_int
 
 n = get_int("Height: ")
 
+if n <= 0:
+    return
+
 for i in range(n):
-    print(" " * (n - (i + 1)) + "*" * (i + 1) + "  " + "*" * (i + 1) + " " * (n - (i + 1)))
+    print(" " * (n - (i + 1)) + "#" * (i + 1) + "  " + "#" * (i + 1) + " " * (n - (i + 1)))
