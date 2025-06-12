91679489f832cdaa43ee84d177d466bfc091c8fb
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-06-12 07:35:18 +0000

    Thu, Jun 12, 2025, 3:35 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,40 +1,32 @@
 from cs50 import get_string
 
-def main():
-    n = get_string("Number: ")
+n = get_string("Number: ")
 
-    if length == 15 and n[0] + n[1] in [34, 37]:
+length = len(n)
+
+sum = 0
+
+for i in range(length):
+    m = int(n[i])
+    if i % 2 == 0:
+
+        if  m * 2 >= 10:
+            sum += m * 2 // 10 + m * 2% 10
+        else:
+            sum += m * 2
+
+    else:
+        sum += m
+
+print(sum)
+if sum % 10 == 0:
+    if length == 15 and n[0] + n[1] in ["34", "37"]:
         print("AMEX")
-    elif length == 16 and n[0] + n[1] in [51, 52, 53, 54, 55]:
+    elif length == 16 and n[0] + n[1] in ["51", "52", "53", "54", "55"]:
         print("MASTERCARD")
-    elif length == 13 or length == 16 and n[0] == 4:
+    elif length == 13 or length == 16 and n[0] == "4":
         print("VISA")
 
 else:
     print("INVALID")
 
-
-def isvalid(n):
-    length = len(n)
-    sum = 0
-
-    for i in range(length):
-        m = int(n[i])
-        if i % 2 == 0:
-
-            if  m * 2 >= 10:
-                sum += m * 2 // 10 + m * 2% 10
-            else:
-                sum += m * 2
-
-        else:
-            sum += m
-
-    if sum % 10 == 0:
-        return True
-    else
-        return False
-
-
-
-main()
