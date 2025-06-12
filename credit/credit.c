87e7f3bdda810c5ec805443ff958e0fcb8bee9fd
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-06-12 07:37:40 +0000

    Thu, Jun 12, 2025, 3:37 PM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -24,24 +24,26 @@ int main(void){
 
     }
 
-    if(sum % 10 == 0){
+    printf("%i", sum);
 
-        if((l == 13 || l == 16) && f == 4)
-            printf("VISA\n");
+    // if(sum % 10 == 0){
 
-        else if(l == 15 && f == 3 && (s == 4 || s == 7))
-            printf("AMEX\n");
+    //     if((l == 13 || l == 16) && f == 4)
+    //         printf("VISA\n");
 
-        else if(f == 5 && s <= 5)
-            printf("MASTERCARD\n");
+    //     else if(l == 15 && f == 3 && (s == 4 || s == 7))
+    //         printf("AMEX\n");
 
-        else
-            printf("INVALID\n");
+    //     else if(f == 5 && s <= 5)
+    //         printf("MASTERCARD\n");
 
-    }
+    //     else
+    //         printf("INVALID\n");
 
-    else
-        printf("INVALID\n");
+    // }
+
+    // else
+    //     printf("INVALID\n");
 
 }
 
