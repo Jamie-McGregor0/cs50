b2efbe733840b583dc6d28dc7411a19ede9affa4
Jamie-McGregor0 <jamiexie06@outlook.com>
2025-06-12 07:58:56 +0000

    Thu, Jun 12, 2025, 3:58 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,8 +6,8 @@ length = len(n)
 
 sum = 0
 
-for i in range(length, -1, -1):
-    m = int(n[i])
+for i in range(length, 0, -1):
+    m = int(n[i - 1])
     if (length - i) % 2 == 0:
             sum += m * 2 // 10 + m * 2 % 10
 
